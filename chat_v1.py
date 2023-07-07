0129d459ef10c5ec44bd2a1ec557b826fee4dce6
zs <lightondust@gmail.com>
2023-07-07 23:11:49 +0900

    save and load conversation

--- chat_v1.py
+++ chat_v1.py
@@ -1,12 +1,20 @@
+import os.path
+
 from openai_api import OpenAI_API
 from conversation import Conversation
 from system_message import system_message
 from auth import get_auth
 
 
-def initialize_conversation(api_key, model, system_message):
+def initialize_conversation(api_key, model, system_message, from_file=None):
     api = OpenAI_API(api_key, model)
-    conversation = Conversation(system_message)
+    conversation = Conversation(system_message, from_file=from_file)
+
+    if from_file:
+        messages = conversation.get_conversation()
+        for m in messages:
+            print_message(m['content'], m['role'])
+
     return api, conversation
 
 
@@ -14,13 +22,38 @@ def handle_user_input(api, conversation, user_message):
     conversation.add_message("user", user_message)
     ai_reply = api.generate_reply(conversation)
     conversation.add_message("assistant", ai_reply)
-    print(ai_reply)
+    print_message(ai_reply, 'assistant')
+    conversation.save_conversation(directory=save_to)
 
 
-# Usage:
-api, conversation = initialize_conversation(get_auth()['token'], 'gpt-3.5-turbo', system_message)
+def print_message(message, role):
+    if role == 'assistant':
+        print()
+        print('-------------------------')
+        print(message)
+    elif role == 'user':
+        print()
+        print('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+        print('your message: ')
+        print(message)
 
-while True:
-    # When a user message comes in
-    user_message = input("Your message: ")
-    handle_user_input(api, conversation, user_message)
+
+if __name__ == '__main__':
+    save_to = 'data/conversations'
+
+    conversation_id = input('Conversation id:')
+    from_file = '{}/{}.json'.format(save_to, conversation_id)
+    if not os.path.exists(from_file):
+        from_file = None
+        conversation_id = None
+
+    api, conversation = initialize_conversation(get_auth()['token'], 'gpt-3.5-turbo', system_message, from_file=from_file)
+
+    print('conversation start({}):'.format(conversation.id))
+
+    while True:
+
+        # When a user message comes in
+        print_message('', 'user')
+        user_message = input()
+        handle_user_input(api, conversation, user_message)
--- conversation.py
+++ conversation.py
@@ -1,16 +1,42 @@
+from datetime import datetime
+import uuid
+import os
+import json
+
+
 class Conversation:
-    def __init__(self, initial_system_message):
-        self.conversation = [{"role": "system", "content": initial_system_message}]
+    def __init__(self, initial_system_message=None, id=None, from_file=None):
+        if from_file:
+            with open(from_file, 'r') as infile:
+                self.conversation = json.load(infile)
+            self.id = os.path.splitext(os.path.basename(from_file))[0]
+        else:
+            self.id = str(uuid.uuid4()) if id is None else id
+            self.conversation = [{"role": "system", "content": initial_system_message, "time": datetime.now().isoformat()}]
 
-    def add_message(self, role, content):
+    def add_message(self, role, content, time=None):
         assert role in ["system", "user", "assistant"], "Invalid role, should be 'system', 'user', or 'assistant'"
-        self.conversation.append({"role": role, "content": content})
+        if time is None:
+            time = datetime.now().isoformat()
+        self.conversation.append({"role": role, "content": content, "time": time})
 
-    def update_system_message(self, content):
+    def update_system_message(self, content, time=None):
         for message in self.conversation:
             if message['role'] == 'system':
                 message['content'] = content
+                if time is not None:
+                    message['time'] = time
                 break
 
-    def get_conversation(self):
-        return self.conversation
+    def get_conversation(self, for_db=False):
+        if for_db:
+            return self.conversation
+        else:
+            return [{"role": message["role"], "content": message["content"]}
+                    for message in self.conversation]
+
+    def save_conversation(self, directory="data/conversations"):
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+        with open(os.path.join(directory, f"{self.id}.json"), 'w') as outfile:
+            json.dump(self.get_conversation(for_db=True), outfile)
